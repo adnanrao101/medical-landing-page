58831a979f7995420b1ca0642c988a3daca4ca47
Adnan Rao <adnanejaz2874@gmail.com>
2025-09-02 16:51:11 +0500

    navbar done

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,52 +1,52 @@
 import './navbar.css'
 import Logo from './City-Dental-Care.svg'
-function Navbar(){
-    return(
+function Navbar() {
+    return (
         <>
 
-        <header>
-            <div id='d1'>
-                <div id="md1">
-                <img src={Logo} alt="logo" />
-                </div>
-            </div>
-            <div id='d2'>
-                <div class="md2">
-                    <div class='md3'>
-                <ion-icon name="time-outline"></ion-icon>
-                    </div>
-
-                    <div class="md4">
-                <p>Opening Time</p>
-                <h5>Mon - Fri 8:30AM - 6PM</h5>
+            <header>
+                <div id='d1'>
+                    <div id="md1">
+                        <img src={Logo} alt="logo" />
                     </div>
                 </div>
-                <div class="md2">
+                <div id='d2'>
+                    <div class="md2">
+                        <div class='md3'>
+                            <ion-icon name="time-outline"></ion-icon>
+                        </div>
 
-                    <div class='md3'>
-                    <ion-icon name="call-outline"></ion-icon>
+                        <div class="md4">
+                            <span>Opening Time</span>
+                            <span>Mon-Fri 8:30AM-6PM</span>
+                        </div>
                     </div>
+                    <div class="md2">
 
-                    <div class='md4'>
-                    <p>Phone</p>
-                    <h5>+9211111111</h5>
+                        <div class='md3'>
+                            <ion-icon name="call-outline"></ion-icon>
+                        </div>
+
+                        <div class='md4'>
+                            <span>Phone</span>
+                            <span>+9211111111</span>
+                        </div>
+
                     </div>
+                    <div class="md2">
+                        <div class='md3'>
+                            <ion-icon name="location-outline"></ion-icon>
+                        </div>
 
+                        <div class="md4">
+                            <span>Location</span>
+                            <span>9 Earl Grey St, United Kingdom</span>
+                        </div>
+                    </div>
                 </div>
-                <div class="md2">
-                    <div class='md3'>
-                    <ion-icon name="location-outline"></ion-icon>
-                    </div>
+            </header>
 
-                    <div class="md4">
-                    <p>Location</p>
-                    <h5>9 Earl Grey St, United Kingdom</h5>
-                    </div>
-                </div>
-            </div>
-        </header>
-
-        <hr />
+            <hr />
 
         </>
     )
